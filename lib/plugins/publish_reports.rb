e7b46dcaedd466cf1fb68750c991fc5a87f08d45
Diego Queiroz <dqueiroz@quintoandar.com.br>
2017-07-28 16:01:10 -0300

    Adding plugin to publish to S3 and pagerduty

---
+++ lib/plugins/publish_reports.rb
@@ -0,0 +1,54 @@
+require 'httparty'
+require 'aws-sdk'
+
+PUBLISH_REPORTS_TESTS_FAILED = []
+
+AfterConfiguration do |config|
+  config.on_event :after_test_case do |event|
+    if not event.result.ok?
+      PUBLISH_REPORTS_TESTS_FAILED.push(event.test_case.name)
+    end
+  end
+end
+
+at_exit do
+  bucket = ENV['REPORT_BUCKET']
+  bucket_region = ENV['REPORT_BUCKET_REGION'] || 'us-east-1'
+  report_url = nil
+  if bucket && system('allure --help >> /dev/null')
+    system('allure generate --clean reports')
+    Dir[File.join(Dir.pwd, "allure-report/**/*")].each do |file|
+      if not File.directory?(file)
+        file_name = file.clone
+        file_name.sub! File.join(Dir.pwd, "allure-report/"), ''
+        s3 = Aws::S3::Resource.new(region: bucket_region)
+        obj = s3.bucket(bucket).object("#{BUILD}/#{file_name}")
+        obj.upload_file(file)
+        obj.acl.put({ acl: "public-read" })
+      end
+    end
+    report_url = "http://#{bucket}.s3-website-#{bucket_region}.amazonaws.com/#{URI.escape(BUILD)}"
+  end
+  if ENV['PAGERDUTY_ROUTING_KEY'] && PUBLISH_REPORTS_TESTS_FAILED.length
+    payload = {
+      :payload => {
+        :summary => "#{PUBLISH_REPORTS_TESTS_FAILED.length} #{PROJECT_NAME} test(s) failed",
+        :source => "#{PROJECT_NAME}",
+        :severity => "critical",
+        :custom_details => "Failed test(s):\n- #{PUBLISH_REPORTS_TESTS_FAILED.join("\n- ")}"
+      },
+      :event_action => "trigger",
+      :routing_key => ENV['PAGERDUTY_ROUTING_KEY'],
+    }
+    if report_url
+      payload[:links] = [{
+        :href => report_url,
+        :text => "Report for #{BUILD}"
+      }]
+    end
+    response = HTTParty.post "https://events.pagerduty.com/v2/enqueue",
+                        headers:{ "Content-Type" => "application/json"},
+                        body: payload.to_json
+  end
+end
+
