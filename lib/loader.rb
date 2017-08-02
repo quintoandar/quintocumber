67b819391941839b33dcd9b1bd6655d9b17ac786
Diego Queiroz <dqueiroz@quintoandar.com.br>
2017-08-02 20:27:21 -0300

    Better loader order

--- lib/loader.rb
+++ lib/loader.rb
@@ -6,6 +6,10 @@ Dir[File.join(File.dirname(__FILE__), "plugins/**/*.rb")].each do |file|
   require file 
 end
 
+Dir[File.join(Dir.pwd, "features/support/**/*.rb")].each do |file|
+  require file 
+end
+
 Dir[File.join(Dir.pwd, "features/**/*.rb")].each do |file|
   require file 
 end
