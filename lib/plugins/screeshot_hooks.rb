be0889c66d949093b45d517da88f0acb3fa6356a
Diego Queiroz <dqueiroz@quintoandar.com.br>
2017-08-29 18:42:26 -0300

    Fixing broken cucumber api

--- lib/plugins/screeshot_hooks.rb
+++ lib/plugins/screeshot_hooks.rb
@@ -6,14 +6,16 @@ class AllureCucumberDSLTemp
 end
 
 AfterConfiguration do |config|
-  config.on_event :after_test_step do |event|
-    if !ENV['DISABLE_REPORTS_SCREENSHOT'] && !event.result.ok?
-      filename = "#{event.test_case.name}/#{event.test_step.name}.png"
-      Capybara.page.save_screenshot(filename)
-      AllureCucumberDSLTemp.attach_file(
-        filename,
-        File.open(File.join(Dir.pwd, Capybara.save_path, filename.to_s))
-      )
+  config.on_event :test_case_finished do |event2|
+    config.on_event :test_step_finished do |event|
+      if !ENV['DISABLE_REPORTS_SCREENSHOT'] && !event.result.ok?
+        filename = "#{event2.test_case.name}/#{event.test_step.name}.png"
+        Capybara.page.save_screenshot(filename)
+        AllureCucumberDSLTemp.attach_file(
+          filename,
+          File.open(File.join(Dir.pwd, Capybara.save_path, filename.to_s))
+        )
+      end
     end
   end
 end
