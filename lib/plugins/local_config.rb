87faad3b108e9f9a15ba4762de8407997507303b
danielavieira102 <danielavieira102@gmail.com>
2017-10-12 15:53:50 -0300

    Update local_config.rb
    Updating the syntax from "if"

--- lib/plugins/local_config.rb
+++ lib/plugins/local_config.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Capybara.register_driver :wip do |app|
-  test_browser = ENV['TEST_BROWSER'] || 'firefox'
+  test_browser = ENV['TEST_BROWSER'] || 'chrome'
   selenium_server = ENV['SELENIUM_SERVER']
-  if selenium_server == "":
+  if selenium_server == ""
     Capybara::Selenium::Driver.new(app, browser: test_browser.to_sym)
   else
     Capybara::Selenium::Driver.new(app, browser: remote, url: selenium_server, desired_capabilities: test_browser)
