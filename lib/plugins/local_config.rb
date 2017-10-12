3efc6467fff58c7a1de207bb2d0b5fd4b7f58091
Fernando Barbosa <fbcbarbosa@gmail.com>
2017-10-12 15:31:22 -0300

    Update local_config.rb

--- lib/plugins/local_config.rb
+++ lib/plugins/local_config.rb
@@ -2,5 +2,10 @@
 
 Capybara.register_driver :wip do |app|
   test_browser = ENV['TEST_BROWSER'] || 'firefox'
-  Capybara::Selenium::Driver.new(app, browser: test_browser.to_sym)
+  selenium_server = ENV['SELENIUM_SERVER']
+  if selenium_server == "":
+    Capybara::Selenium::Driver.new(app, browser: test_browser.to_sym)
+  else
+    Capybara::Selenium::Driver.new(app, browser: remote, url: selenium_server, desired_capabilities: browser)
+  end
 end
