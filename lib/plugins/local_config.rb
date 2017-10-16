161abe0880c33ab102cefe21bca3e06aef138f8f
Daniela <danielavieira102@gmail.com>
2017-10-16 18:44:45 -0200

    Now you can run watcher in a remote_server passing SELENIUM_SERVER env

--- lib/plugins/local_config.rb
+++ lib/plugins/local_config.rb
@@ -2,11 +2,10 @@
 
 Capybara.register_driver :wip do |app|
   test_browser = ENV['TEST_BROWSER'] || 'chrome'
-  puts test_browser
   selenium_server = ENV['SELENIUM_SERVER']
-  if selenium_server == ""
+  if ENV['SELENIUM_SERVER'].nil?
     Capybara::Selenium::Driver.new(app, browser: test_browser.to_sym)
   else
-    Capybara::Selenium::Driver.new(app, browser: remote, url: selenium_server, desired_capabilities: test_browser)
+    Capybara::Selenium::Driver.new(app, browser: :remote, url: selenium_server, desired_capabilities: test_browser.to_sym)
   end
 end
