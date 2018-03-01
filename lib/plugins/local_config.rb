4703eef21fcc8cde11d0068406dbecddcc919ae5
Fernando Barbosa <fernando.barbosa@quintoandar.com.br>
2018-03-01 18:52:06 -0300

    Add headless chrome driver option

--- lib/plugins/local_config.rb
+++ lib/plugins/local_config.rb
@@ -3,8 +3,15 @@
 Capybara.register_driver :wip do |app|
   test_browser = ENV['TEST_BROWSER'] || 'chrome'
   selenium_server = ENV['SELENIUM_SERVER']
-  if ENV['SELENIUM_SERVER'].nil?
+  if selenium_server.nil?
     Capybara::Selenium::Driver.new(app, browser: test_browser.to_sym)
+  elsif test_browser == 'headless_chrome':
+      capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+        chromeOptions: { args: %w(headless disable-gpu) }
+      )
+    Capybara::Selenium::Driver.new app,
+      browser: :remote,
+      desired_capabilities: capabilities
   else
     Capybara::Selenium::Driver.new(app, browser: :remote, url: selenium_server, desired_capabilities: test_browser.to_sym)
   end
